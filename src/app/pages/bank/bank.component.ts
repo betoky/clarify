d346afe574f10be38632edff7684e9a74f06ead1
Betoky <--global>
2024-08-02 15:40:06 +0300

    more transaction list

--- src/app/pages/bank/bank.component.ts
+++ src/app/pages/bank/bank.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -18,6 +18,7 @@ import { TransactionRegisterComponent } from '../../components/transactions/regi
 import { TransactionItemComponent } from '../../components/transactions/item/item.component';
 import { Transaction } from '../../models/transaction';
 import { TransactionListComponent } from '../../components/transactions/list/list.component';
+import { Paginate } from '../../models/pagination';
 
 @Component({
     selector: 'app-bank',
@@ -37,32 +38,34 @@ import { TransactionListComponent } from '../../components/transactions/list/lis
     templateUrl: './bank.component.html',
     styleUrl: './bank.component.scss'
 })
-export class BankComponent implements OnInit {
+export class BankComponent {
     private balanceService = inject(BalanceService);
     private transactionService = inject(TransactionsService);
     private dummyService = inject(DummyService);
-
     readonly isLocalMode = environment.runningMode === 'local';
+    private transactionsPagination: Paginate = {
+        max: 10,
+        orderby: 'date',
+        sortby: 'DESC'
+    }
+
     balance = this.balanceService.balance;
-    listMode: 'last' | 'all' = 'last';
+
+    lastTransactionMode = true;
     lastTransactions = toSignal(this.transactionService.lastTransactions());
+    listTransactions = signal([] as Transaction[]);
 
     readonly range = new FormGroup({
         start: new FormControl<Date>(new Date()),
         end: new FormControl<Date >(new Date()),
     });
 
-    ngOnInit(): void {
-        this.transactionService.getTransactionByOperation('withdral')
-        // .then(data => console.log(data.data()))
+    showMoreList() {
+        this.lastTransactionMode = false;
+        this.transactionService.getTransactions(this.transactionsPagination)
+        .then(data => this.listTransactions.set(data))
     }
-
-    onFilter() {
-        // console.log('++ Range', this.range.value);
-    }
-    toggleListMode() {
-        this.listMode = this.listMode === 'last' ? 'all' : 'last';
-    }
+    
     generateFakeTransactions() {
         this.dummyService.generateTransactions(50).then(data => {
             data.forEach(doc => {
